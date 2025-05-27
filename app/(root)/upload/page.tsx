2d1a0687f127ded372633580e5d9d60ba5abd899
sumit-saurabh98 <sumitsaurabh112@gamil.com>
2025-05-27 09:31:12 +0530

    added form

--- app/(root)/upload/page.tsx
+++ app/(root)/upload/page.tsx
@@ -1,10 +1,15 @@
 "use client";
 import FileInput from "@/components/FileInput";
 import FormField from "@/components/FormField";
-import React, { useState } from "react";
+import { MAX_THUMBNAIL_SIZE, MAX_VIDEO_SIZE } from "@/constants";
+import { useFileInput } from "@/lib/hooks/useFileInput";
+import React, { FormEvent, useState } from "react";
 
 const page = () => {
-  const [error, setError] = useState(null);
+    const video = useFileInput(MAX_VIDEO_SIZE);
+    const thumbnail = useFileInput(MAX_THUMBNAIL_SIZE);
+    const [isSubmitting, SetIsSubmitting] = useState(false);
+  const [error, setError] = useState("");
   const [formData, setFormData] = useState({
     title: "",
     description: "",
@@ -16,11 +21,40 @@ const page = () => {
     setFormData((prevState) => ({ ...prevState, [name]: value }));
   };
 
+  const handleSubmit = (e:FormEvent) => {
+    e.preventDefault();
+
+    SetIsSubmitting(true);
+
+    try {
+        if(!video.file || !thumbnail.file){
+            setError("Please upload video and thumbnail");
+            return;
+        }
+
+        if(!formData.title || !formData.description){
+            setError("Please enter title and description");
+            return;
+        }
+
+        //upload the video to bunny
+        //upload the thumbnail to DB
+        // Attach the thumabnail to the video
+        //create a new db entry for the video details(url, data)
+
+
+    } catch (error) {
+        console.log("Error in submitting form",error);
+    }finally{
+        SetIsSubmitting(false);
+    }
+  }
+
   return (
     <div className="wrapper-md upload-page">
       <h1>Upload a video</h1>
       {error && <div className="error-field">{error}</div>}
-      <form className="rounded-20 shadow-10 gap-6 w-full flex flex-col px-5 py-7.5">
+      <form className="rounded-20 shadow-10 gap-6 w-full flex flex-col px-5 py-7.5" onSubmit={handleSubmit}>
         <FormField
           id="title"
           label="Title"
@@ -36,8 +70,28 @@ const page = () => {
           onChange={handleInputChange}
           placeholder="Describe what this video is about"
         />
-        <FileInput/>
-        <FileInput />
+        <FileInput
+          id="video"
+          label="Video"
+          accept="video/*"
+          file={video.file}
+          previewUrl={video.previewUrl}
+          inputRef={video.inputRef}
+          onChange={video.handleFileChange}
+          onReset={video.resetFile}
+          type="video"
+        />
+        <FileInput
+          id="thumbnail"
+          label="Thumabnail"
+          accept="image/*"
+          file={thumbnail.file}
+          previewUrl={thumbnail.previewUrl}
+          inputRef={thumbnail.inputRef}
+          onChange={thumbnail.handleFileChange}
+          onReset={thumbnail.resetFile}
+          type="image"
+        />
         <FormField
           id="visibility"
           label="Visibility"
@@ -49,6 +103,9 @@ const page = () => {
             { value: "private", label: "Private" },
           ]}
         />
+        <button type="submit" disabled={isSubmitting} className="submit-button">
+            {isSubmitting ? "Uploading...":"Upload Video"}
+            </button>
       </form>
     </div>
   );
--- components/FileInput.tsx
+++ components/FileInput.tsx
@@ -1,10 +1,41 @@
+import Image from 'next/image'
 import React from 'react'
 
-const FileInput = () => {
+const FileInput = ({id, label, accept, file, previewUrl, inputRef, onChange, onReset, type}:FileInputProps) => {
   return (
-    <div>
-      FILE input
-    </div>
+    <section className='file-input'>
+        <label htmlFor={id}>{label}</label>
+
+        <input
+          id={id}
+          type="file"
+          accept={accept}
+          ref={inputRef}
+          onChange={onChange}
+          hidden
+        />
+
+        {!previewUrl ? (
+            <figure onClick={() => inputRef.current?.click()}>
+                <Image src={'/assets/icons/upload.svg'} alt='upload' width={24} height={24}/>
+                <p>Click to upload your {id}</p>
+            </figure>
+        ): (
+            <div>
+                {
+                    type === 'video' ? (
+                        <video src={previewUrl} controls></video>
+                    ): (
+                        <Image src={previewUrl} alt="image" fill/>
+                    )
+                }
+                <button type='button' onClick={onReset}>
+                    <Image src={'/assets/icons/close.svg'} alt='close' width={16} height={16}/>
+                </button>
+                <p>{file?.name}</p>
+            </div>
+        )}
+    </section>
   )
 }
 
